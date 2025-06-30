05478ee30202ff7f773885553f16289c6000fd7b
chiappettafran <franciscomartinezchiappetta@gmail.com>
2025-06-30 17:04:07 -0300

    soporte para https sin ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    https: {
+      key: "./localhost-key.pem",
+      cert: "./localhost.pem",
+    },
+  }
 });
